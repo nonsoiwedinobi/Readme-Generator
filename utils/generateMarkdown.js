987e28577e11d30ce44d10542a848e5175ab513e
Nonso <nonsoiwedinobi@yahoo.com>
2023-12-18 20:19:12 +0000

    Updated the entire project

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,41 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  return `
+# ${answers.title}
 
-`;
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+![${answers.license} License](https://img.shields.io/badge/license-${answers.license}-green)
+
+This project is licensed under the ${answers.license} License.
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+For any questions, please contact [${answers.username}](https://github.com/${answers.username}) or email at ${answers.email}.
+`; 
+
+;
 }
 
 module.exports = generateMarkdown;
